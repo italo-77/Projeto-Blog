0e9966c46892b75e3cc0abc05db794c46cf42bad
Italo Lopes <188580021+italo-77@users.noreply.github.com>
2025-06-15 18:38:58 -0300

    Create auth-callback.js

---
+++ netlify/functions/auth-callback.js
@@ -0,0 +1,13 @@
+const { createHandlers } = require('netlify-cms-oauth-provider-node');
+
+const { complete } = createHandlers(
+  {
+    origin: 'https://meu-blog-cms.netlify.app',
+    completeUrl: 'https://meu-blog-cms.netlify.app/.netlify/functions/auth-callback',
+    oauthClientID: process.env.GITHUB_CLIENT_ID,
+    oauthClientSecret: process.env.GITHUB_CLIENT_SECRET,
+  },
+  { useEnv: true }
+);
+
+exports.handler = complete;
